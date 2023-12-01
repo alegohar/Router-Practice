0953f76da88ccdab3afe75a587c71bb14263cbea
alegohar <alegohar@gmail.com>
2023-12-01 23:27:38 +0500

    Routing Practice

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RouteMenu from './Router/Router'
+function App() {
+  
+  return (
+    <>
+  <div>asd</div>
+  <div>asd</div>
+  </>
+  )
+}
+
+export default App
---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { ReactDOM } from "react";
+import {Routes, Route} from "react-router-dom"
+import Home from "../pages/Home";
+import About from "../pages/About";
+function RouteMenu ()
+{
+    return (<Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+        </Routes>
+         )
+}
+export default RouteMenu;
